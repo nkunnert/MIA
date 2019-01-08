ffb145436162491e478a00adeb8498c82eecd33e
DESKTOP-PUFOV0N\Gebruiker <broitspowns@gmail.com>
2019-01-08 12:42:10 +0100

    Added code to communicate with the IoTMessage API from Isah using a PUT method

--- MIA_ESP/MIA_ESP.ino
+++ MIA_ESP/MIA_ESP.ino
@@ -3,8 +3,8 @@
 
 int requestIndex = 0;
  
-const char* ssid = "MIAWIFI";
-const char* password = "12345698";
+const char* ssid = "Yeet";
+const char* password = "MIA2018x";
 
 const String requestUrl = "http://miatest.vanoort.eu";
 const String loginToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpc3MiOiJJc2FoIEF1dGhlbnRpY2F0aW9uIFNlcnZpY2UiLCJhdWQiOiJJc2FoIFdlYiBTZXJ2aWNlcyIsIm5iZiI6MTQ0ODAyNTY3MSwiZXhwIjoxNjcyNTIyMjAwMCwiSXNhaFVzZXJOYW1lIjoiTWFjaGluZU9wZXJhdGlvbnMiLCJNYWNoaW5lQXV0aGVudGljYXRpb24iOiJ0cnVlIn0.C9zrjyBstUNb3hK1e3kiAVYM1hwer09mbm5VO/b6fdstHoW3G1tAMrZA9n8ElEkqYqBkJCzs46QKnp1kawyNdZbyahsfYEBZ7czRoLCVr74ozwI6yC8Do1LwRO820voTBa+bQlHEh0nGlj++k+5odX/MIVf1eX9VkgBqAjOUbITRVwIT5ia260t5ZC0UI9gy1xr7k5wievliLSkrAIVmX++7nQOUzZgKOlHSu3/vmuBA1PCPOtE6l4brnrBc04ohzBAKt3CfZG4gXxeeVOwHPd1YEzSaePzGKlo725Zwk4/u44mDiee2fGfPAzjBLcL2radEJ5OAHjZuIuGIYambJZ+oDGmGUy55Fy/RImd6secT3DJhx5TIgBbEjPiDje32Q9BYAvogplHvzlzpHI9tOCQXG44APjxN9PjW1fFy3+7cVixgMM9zGwRHkeAjNSQluO+GXOygcjIr11GW4moZVY5ak2IOuM127FBfPJLnmKT8Lb0GgLnBpNXeV4rtKkemSYTRheeF8GcDHgWdR29S+RJT3MhvdEPpR3JDox6EgiuttbUd3fAFZD8u98iqkjn/zZxKsQY2io4bFfDfdmZXM9cTMKt8IlADdsM5jOm0txgvjd11cFfSlwp4A1tpRrXNMGw03ypMZ2DY9Lk+msSZEnIzDkJJ3illbDXBHZ5iGDI=";
@@ -50,7 +50,8 @@ void loop() {
     setAuthToken(newToken);
   }
 
-  getOperations();
+  //getOperationsRaw();
+  //sendIotMessage("TEST", "Test bericht", "50", "Degrees");
   delay(5000);
 }
 
@@ -73,7 +74,6 @@ String getAuthToken(){
 
     http.begin(requestUrl + ":3087/MachineOperationService/users/" + sfcUsername + "/unsecure/token");
     http.addHeader("Authorization", loginToken);
-    //http.addHeader("Content", "");
     http.addHeader("Content-Type", "application/xml");
     http.addHeader("cache-control", "no-cache");
 
@@ -117,5 +117,27 @@ String getOperationsRaw(){
     http.end();
   }
   
-  return null;
+  return "";
+}
+
+void sendIotMessage(String type, String message, String value, String unit){
+  if(WiFi.status()== WL_CONNECTED){
+    String payload = "";
+    HTTPClient http;
+
+    http.begin(requestUrl + ":3099/api/IoTMessage/");
+    http.addHeader("Content-Type", "application/json");
+
+    String body = "{\n\t\"MessageType\": \"" + type + "\",\n\t\"Description\": \"" + message + "\",\n\t\"Value\": \"" + value + "\",\n\t\"" + unit + "\": \"St\",\n\t\"MessageDate\": \"\"\n}";
+    int httpCode = http.PUT(body);
+
+    if (httpCode > 0) { //Check the returning code
+ 
+      payload = http.getString();
+      Serial.println(payload);
+      
+    }
+
+    http.end();
+  }
 }
